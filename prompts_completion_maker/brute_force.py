ee85a60baa4d28c1581d4bea2abcff467c3465e2
yusuf-74 <yachour31@gmail.com>
2023-03-10 05:40:19 +0200

    combine all data in one file 'all_done'

--- prompts_completion_maker/brute_force.py
+++ prompts_completion_maker/brute_force.py
@@ -1,11 +1,21 @@
 import json
 
-ans = ""
+data = list()
 
-for i in range(5,11):
-    with open(f"prompts_completion_maker/Q{i}_bot.json", "r") as read_file:
-        data = json.load(read_file)
-        ans += data["content"]
-with open(f"prompts_completion_maker/all_bot.txt", "w") as write_file:
-    write_file.write(ans)
-        
+for i in range(1,5):
+    try:
+        with open (f"prompts_completion_maker/Q{i}_me.json","r") as file:
+            file_data = json.load(file)
+            data += file_data
+    except:
+        print(f"file : {i}")
+for i in range(5,84):
+    try:
+        with open (f"prompts_completion_maker/Q{i}_bot.json","r") as file:
+            file_data = json.load(file)
+            data += file_data
+    except:
+        print(f"file : {i}")
+
+with open (f"prompts_completion_maker/all_done.json","w") as file:
+    json.dump(data,file,indent=4)
