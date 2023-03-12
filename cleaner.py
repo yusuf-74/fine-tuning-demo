92b2c27483734624b3a7e8699c399098050d73ab
yusuf-74 <yachour31@gmail.com>
2023-03-12 02:40:05 +0200

    cleaning paragraphs

--- cleaner.py
+++ cleaner.py
@@ -1,7 +1,18 @@
 import json
 
 cleaned_paragraphs = []
+temp = []
+seen = set()
 
+def check_alphanumeric(string):
+    alpha = False
+    num = False
+    for char in string:
+        if char.isdigit():
+            num = True
+        if char.isalpha():
+            alpha = True
+    return num & alpha
 
 with open('paragraphs.json') as f:
     paragraphs = json.load(f)
@@ -9,7 +20,16 @@ with open('paragraphs.json') as f:
 for paragraph in paragraphs:
     for sentence in paragraph:
         if len(sentence) > 700:
-            cleaned_paragraphs.append(sentence)
+            temp.append(sentence)
 
 with open('cleaned_paragraphs.json','w') as f:
-    json.dump(cleaned_paragraphs,f,indent=4)
+    for sentence in temp:
+        x = sentence.split(' ')
+        if check_alphanumeric(x[0]):
+            x[0] = 'course of'
+        paragraph = ' '.join(x)
+        if paragraph not in seen:
+            seen.add(paragraph)
+            cleaned_paragraphs.append(paragraph)
+            
+    json.dump(cleaned_paragraphs[:18]+cleaned_paragraphs[19:38]+cleaned_paragraphs[39:],f,indent=4)
